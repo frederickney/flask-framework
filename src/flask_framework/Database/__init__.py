81b2278e0f7fd72044c5d732e44fc09635c9d162
= <frederick.ney@gmail.com>
2026-03-13 16:43:05 +0100

    adding safe ref to database module on top level

--- src/flask_framework/Database/__init__.py
+++ src/flask_framework/Database/__init__.py
@@ -3,6 +3,7 @@
 
 __author__ = 'Frederick NEY'
 
-from .driver import Driver as Database
 from flask_framework.Deprecation import module_deprecation
 module_deprecation(__name__, __name__.lower(), '1.3.0')
+from .driver import Driver as Database
+from .decorators import safe
