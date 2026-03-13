6bba0af75b33c71e9b5186caa7015412e1fc3f8f
= <frederick.ney@gmail.com>
2026-03-13 12:05:56 +0100

    remove make_auth helper

--- src/flask_framework/Utils/__init__.py
+++ src/flask_framework/Utils/__init__.py
@@ -4,6 +4,6 @@
 __author__ = "Frederick NEY"
 
 from .module import generate
-from .utils import make_auth, make_controller, make_middleware, make_project
+from .utils import make_controller, make_middleware, make_project
 from flask_framework.Deprecation import module_deprecation
 module_deprecation(__name__, __name__.lower().replace('server', 'core'), '1.3.0')
